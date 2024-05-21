b55f6563df9660a4f23ccf93e21a4e0e2ddcf620
BjornBlog <broberts25@riverdale.k12.or.us>
2024-05-21 16:44:20 -0700

    Started on IBUS Decoding with library free one, added base code,  need to adapt

--- sketch_apr22a/sketch_apr22a.ino
+++ sketch_apr22a/sketch_apr22a.ino
@@ -1,168 +1,128 @@
 #include "sbus.h"
 
-bfs::SbusRx sbus_rx(&Serial1);
+#define IBUS_BUFFSIZE 32    // Max iBus packet size (2 byte header, 14 channels x 2 bytes, 2 byte checksum)
+#define IBUS_MAXCHANNELS 10 // My TX only has 10 channels, no point in polling the rest
+
 /* SBUS object, writing SBUS */
 bfs::SbusTx sbus_tx(&Serial1);
 /* SBUS data */
 bfs::SbusData data;
 bfs::SbusData sbusInput;
+
+
 // int channelValues[] = {1500, 1500, 1500, 885, 1500, 1500, 1500};
-int tman; // pitch is broken?
+static uint8_t ibusIndex = 0;
+static uint8_t ibus[IBUS_BUFFSIZE] = {0};
+static uint16_t rcValue[IBUS_MAXCHANNELS];
+static boolean rxFrameDone;
+int pitch; // pitch is broken?
 int roll;
 int yaw;
 int throttle;
 int arming;
-bool failsafe = false;
 bool enabled = false;
 long t = 0;
+long blinkTime = 0;
 int state = -1;  
-int kill = 1000;
+int killswitch = 1000;
+int failsafe = 1000;
+int LEDpin = 13;
+int compare = 1000;
+bool lightOn = false;
+int rampUpTime = 0;
 /*
+-1: startup
 0: arming state
 1: sending signals
+2: test state
 */
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   while (!Serial) {}
   t = millis();  
+  blinkTime = millis();
   /* Begin the SBUS communication */
   sbus_rx.Begin();
   sbus_tx.Begin();
+
   roll = 1500;
-  tman = 1500;
+  pitch = 1500;
   yaw = 1500;
   throttle = 880;
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println("setup has finished");
 }
 
-void loop() {
-    // roll = channelValues[1];
-    // pitch = channelValues[2];
-    // yaw = channelValues[3];
-    // throttle = channelValues[4];
-    if (state == -1){
-      throttle = 885;
-      Arm(false);     
-      data.ch[0] = (ChannelMath(roll));
-      data.ch[1] = (ChannelMath(tman));
-      data.ch[2] = (ChannelMath(throttle));
-      data.ch[3] = (ChannelMath(yaw));
-      data.ch[4] = (ChannelMath(arming));
-      data.ch[7] = (ChannelMath(1881)); //mutes beeper
-      data.ch[10] = (ChannelMath(2000));
-      data.ch[12] = (ChannelMath(1000));
-      if(millis() - t > 10000) //sending init data for 10 seconds
-      {
-        t = millis();
-        state = 0; //it's time to arm
-      }
-    }
-    else if (state == 0){
-      //set arming signals
-      Arm(true);
-      data.ch[2] = (ChannelMath(throttle));
-      data.ch[4] = (ChannelMath(arming));
-      data.ch[10] = (ChannelMath(1700));
-      if (millis() - t > 10000){
-        state = 1;
-        t = millis();
-      }
-    }
-    else if (state == 1){
-      //Roll Ch 0, Pitch Ch 1, Yaw Ch 3, Throttle Ch 2, Arm Ch 4
-      if(millis() - t < 5000)
-      {
-        roll = 1500;
-        tman = 1500;
-        yaw = 1500;
-        throttle = 885;
-        kill = 1000;
-      }  
-      else if(millis() - t < 15000)
-      {    
-        roll = 1500;
-        tman = 1500;
-        yaw = 1500;
-        throttle = 1400;
-        kill = 1000;
-      }
-      else{
-        roll = 1500;
-        tman = 1500;
-        yaw = 1500;
-        throttle = 885;  
-        Arm(false);      
-        kill = 1700;
-      }
-      
-      data.ch[0] = (ChannelMath(roll));
-      data.ch[1] = (ChannelMath(tman));
-      data.ch[2] = (ChannelMath(throttle));
-      data.ch[3] = (ChannelMath(yaw));
-      data.ch[4] = (ChannelMath(arming));       
-      data.ch[17] = ((988-879.7)/.6251);
-      data.ch[16] = ((988-879.7)/.6251);
-      data.ch[10] = (ChannelMath(2000));
-      data.ch[12] = (ChannelMath(kill));
-    } // end state 1
-    // --- back to the base loop code ---  
-    /* Grab the received data */
-    sbusInput = sbus_rx.data();
-    // data = sbus_rx.data(); THIS STUPID LINE OF CODE MADE ME SPEND LIKE 2.5 WEEKS CHASING GEESE WILDLY
-    /* Display the received data */
-    // Serial.print("\t Output \t");
-    // for (int8_t i = 0; i < data.NUM_CH; i++) {
-    //     data.ch[i] = ((1500-879.7)/.6251);
-    // }
-    Serial.println("Output");
-    for (int8_t i = 0; i < data.NUM_CH; i++){ //print data we're sending.
-        Serial.print(i);
-        Serial.print(": ");
-        if(data.ch[i] != 0)
-        {
-          Serial.println((.6251*(data.ch[i]))+879.7);
-        }
-        else
-        {
-          Serial.println(data.ch[i]);
-        }
-        delay(100);
-    } // close printing for
-    Serial.println("-----");
-    Serial.print("State is: ");
-    Serial.println(state);
-    // ChangeThrottle();
-    Serial.println("Input");
-   for(int i = 0; i < sbusInput.NUM_CH; i++)
+void loop() 
+{
+  ReadRx();
+  if (state == -1){
+    compare = 1000;       
+    throttle = 885;
+    killswitch = 1000;
+    Arm(false);     
+    if(millis() - t > 10000) //sending init data for 10 seconds
     {
-      Serial.print(i);
-      Serial.print(": ");
-      if(/*data.ch[i] != 0*/ false)
-      {
-        Serial.println((.6251*(sbusInput.ch[i]))+879.7);
-      }
-      else
-      {
-        Serial.println(sbusInput.ch[i]);
-      }
-      delay(100);
+      t = millis();
+      state = 0; //it's time to arm
     }
-    // data.ch[4] = ((500-879.7)/.6251);
-    // data.ch[5] = 100.5555;
-    // Serial.println(data);
+  }
+  else if (state == 0){
+    compare = 100;
+    //set arming signals
+    Arm(true);
+    if (millis() - t > 10000){
+      rampUpTime = millis();
+      throttle = 900;
+      state = 2; // 1 is intstant, 2 is slow incraments of 50
+      t = millis();
+    }
+  }
+  else if (state == 1)
+  {
+    //Roll Ch 0, Pitch Ch 1, Yaw Ch 3, Throttle Ch 2, Arm Ch 4
+    /*
+    
+    
+    CONNOR PUT CONTROL HERE
+    
+    
+    */
+  } // end state 1
+  else if(state == 2)
+  {
+    if((millis() - rampUpTime >= 1000) && (killswitch < 1500) && (throttle < 1150))
+    {
+      throttle += 50;
+      rampUpTime = millis();
+    }
+    if((throttle >= 1150) && (millis() - rampUpTime >= 2000))
+    {
+      killswitch = 1700;
+      failsafe = 2000;
+    }
+  }
+  DataSetSend();
+  PrintChannels();
 
-    /* Display lost frames and failsafe data */
-    // Serial.print(data.lost_frame);
-    // Serial.print("\t");
-    // Serial.println(data.failsafe);
-    /* Set the SBUS TX data to the received data */
-    sbus_tx.data(data);
-    // Serial.println(data);
-    /* Write the data to the servos */
-    sbus_tx.Write();
-    delay(100);
+
+  if(state == -1 || state == 0){ //Light Blinker
+    if(millis()- blinkTime >= compare){
+      LightSRLatch();
+      blinkTime = millis();
+    }
+  }
+  else if(killswitch == 1700){
+    digitalWrite(LEDpin,LOW);
+  }
+  else{
+    digitalWrite(LEDpin,HIGH);
+  }
+  delay(100);  
 }
-void Arm(bool armmed)
+
+
+void Arm(bool armmed) // turns arm into a bool
 {
   if(armmed)
   {
@@ -173,13 +133,103 @@ void Arm(bool armmed)
     arming = 1000;
   }
 }
-double ChannelMath(int setpoint)
+
+double ChannelMath(int setpoint) // Sets Sbus data to proper point for conversion
 {
   return((setpoint - 879.7)/.6251);
 }
-// void ChangeThrottle()
-// { 
-//   if (Serial.available() > 0) {
-//   throttle = Serial.read();
-//   }
-// }
+
+void LightSRLatch() // Blinker
+{
+  if(lightOn)
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+    lightOn = false;    
+  }
+  else if(!lightOn)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    lightOn = true;    
+  }
+}
+
+void DataSetSend() // Sends Relivent SBUS channels
+{
+  data.ch[0] = (ChannelMath(roll));
+  data.ch[1] = (ChannelMath(pitch));
+  data.ch[2] = (ChannelMath(throttle));
+  data.ch[3] = (ChannelMath(yaw));
+  data.ch[4] = (ChannelMath(arming));
+  data.ch[7] = (ChannelMath(failsafe)); //mutes beeper NO KONNER ITS FAILSAFE, BEEPERMUTE IS THE SAME AS ARM
+  data.ch[10] = (ChannelMath(2000)); //Signal
+  data.ch[12] = (ChannelMath(killswitch)); //Killswitch
+  sbus_tx.data(data);
+  sbus_tx.Write();
+}
+
+void PrintChannels()
+{
+  Serial.println("lost frame data: " +data.lost_frame +"/n");
+  Serial.println("failsafe data: " +data.failsafe +"/n");
+  Serial.println("Output");
+  for (int8_t i = 0; i < data.NUM_CH; i++){ //print data we're sending.
+      Serial.print(i);
+      Serial.print(": ");
+      if(data.ch[i] != 0)
+      {
+        Serial.println((.6251*(data.ch[i]))+879.7);
+      }
+      else
+      {
+        Serial.println(data.ch[i]);
+      }
+  } // close printing for
+  Serial.println("-----");
+  Serial.print("State is: ");
+  Serial.println(state +"/n" +"-----");
+}
+
+void readRx()
+{
+  rxFrameDone = false;
+  
+  if (Serial.available())
+  {
+    uint8_t val = Serial.read();
+    // Look for 0x2040 as start of packet
+    if (ibusIndex == 0 && val != 0x20)
+    {
+      ibusIndex = 0;
+      return;
+    }
+    if (ibusIndex == 1 && val != 0x40) 
+    {
+      ibusIndex = 0;
+      return;
+    }
+
+    if (ibusIndex == IBUS_BUFFSIZE)
+    {
+      ibusIndex = 0;
+      int high=3;
+      int low=2;
+      for(int i=0;i<IBUS_MAXCHANNELS; i++)
+      {
+        //left shift away the first 8 bits of the first byte and add the whole value of the previous one
+        rcValue[i] = (ibus[high] << 8) + ibus[low];
+        Serial.print(rcValue[i]);
+        Serial.print("     ");
+        high += 2;
+        low += 2;
+      }
+      Serial.println();
+      rxFrameDone = true;
+      return;
+    }
+    else
+    {
+      ibus[ibusIndex] = val;
+      ibusIndex++;
+    }
+  }
+}
