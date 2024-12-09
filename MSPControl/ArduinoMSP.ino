87c2dc10f15aadf342e6cdf1135bd148db52d46d
BjornBlog <116665790+BjornBlog@users.noreply.github.com>
2024-12-09 12:39:29 -0800

    MSP V1

---
+++ MSPControl/ArduinoMSP.ino
@@ -0,0 +1,22 @@
+#include <MSP.h>
+
+MSP msp;
+
+void setup()
+{
+  Serial.begin(115200);
+  msp.begin(Serial);
+}
+
+void loop()
+{
+  msp_rc_t rc;
+  if (msp.request(MSP_RC, &rc, sizeof(rc))) {
+  	
+    uint16_t roll     = rc.channelValue[0];
+    uint16_t pitch    = rc.channelValue[1];
+    uint16_t yaw      = rc.channelValue[2];
+    uint16_t throttle = rc.channelValue[3];
+    
+  }
+}
