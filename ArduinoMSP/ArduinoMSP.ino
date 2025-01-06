00c51481aa8a71fbcafd0660764d01c177caff24
Bjorn Roberts <116665790+bjornwroberts@users.noreply.github.com>
2025-01-06 10:08:46 -0800

    Update ArduinoMSP.ino

--- ArduinoMSP/ArduinoMSP.ino
+++ ArduinoMSP/ArduinoMSP.ino
@@ -1,493 +1,217 @@
-#include <WiFiNINA.h>                    //https://github.com/arduino-libraries/WiFiNINA/tree/master
-#include <WiFiUDP.h>                     
-#include <MSP.h>                         //https://github.com/yajo10/MSP-Arduino
+#define MSP_ATTITUDE 108
+#define MSP_SET_RAW_RC 200
+#define MSP_RAW_GPS 106
 
-MSP msp;
-char ssid[] = "XV_Basestation";          //  network SSID (name)
-int status = WL_IDLE_STATUS;             // the Wi-Fi radio's status
-int ledState = LOW;                      //ledState used to set the LED
-unsigned long previousMillisInfo = 0;    //will store last time Wi-Fi information was updated
-unsigned long previousMillisLED = 0;     // will store the last time LED was updated
-const int intervalInfo = 5000;           // interval at which to update the board information
-char packetBuffer[256];                  //buffer to hold incoming packet
-WiFiUDP Udp;
-unsigned int localPort = 2390;
-char  ReplyBuffer[] = "Drone 1";
-int wifiState = 0;                       //Wifi connection state
-bool firstConnectFrame = false;          //First Loop while connected to wifi
-double pytime = 0;                        //time of python code
-IPAddress bsip;
+uint16_t rc_values[8];
+int loopCount = 0;
 
-double pitch; // pitch is broken?
-double roll;
-double yaw;
-double throttle;
-int arming;
-int updateTime = 0;
-int connectTime = 0;
-long t = 0;
-bool isArmed = false;
-bool isFailsafed = false;
-bool isKilled = false;
-long blinkTime = 0;
-int droneState = -1;  
-int killswitch = 1000;
-int failsafe = 1000;
-int LEDpin = 13;
-int blinkSpeed = 10;
-bool lightOn = false;
-bool bootComplete = false;               //Finished Drone Booting sequence
-bool enabled = false;
-
-struct ManualControlMessage{
-  IPAddress sourceIP;
-  String cmd;
-  double yaw;
-  double pitch;
-  double roll;
-  double throttle;
-  double killswitch;
-  double pytime;
-};
-
-struct BSIPMessage{
-  String cmd;
-  IPAddress BSIP;
-};
-
-void setup()
-{
-  Serial.begin(115200);
-  msp.begin(Serial);
-  if(Serial)
-  {
-    serialUSB = true;
-    Serial.println("Setup");
-  }
-  delay(1000);
-  msp_set_raw_rc_t rcT;
-
-  delay(1000);
-  // set the LED as output
-  pinMode(LED_BUILTIN, OUTPUT);
-  sbus_tx.Begin();
-  roll = 1500;
-  pitch = 1500;
-  yaw = 1500;
-  throttle = 885;
-  WifiConnection();
-}//end setup
-
-void loop()
-{
-  msp_rc_t rcR;
-  msp_set_raw_rc_t rcT;
-  if (msp.request(MSP_RC, &rcR, sizeof(rcR))) {
-  	
-    uint16_t inputRoll     = rcR.channelValue[0];
-    uint16_t inputPitch    = rcR.channelValue[1];
-    uint16_t inputYaw      = rcR.channelValue[2];
-    uint16_t inputThrottle = rcR.channelValue[3]; 
-  }
-  status = WiFi.status();
-//   if(status != WL_CONNECTED)
-//   {  
-//     blinkSpeed = 50;
-//     failsafe = 2000;
-//   }
-//   else
-//   {
-//     failsafe = 1000;
-//   }
-  //MillisStuff();
-  WifiConnection();//checks the wi-fi status
-  Listen();
-  DroneSystems();
-  rcT.channelValue[0] = roll;
-  rcT.channelValue[1] = pitch; 
-  rcT.channelValue[2] = throttle;
-  rcT.channelValue[3] = yaw;
-  rcT.channelValue[4] = arming;
-  rcT.channelValue[5] = 1500;
-  rcT.channelValue[6] = 1500;
-  rcT.channelValue[7] = failsafe;
-  rcT.channelValue[8] = 1500;
-  rcT.channelValue[9] = 1500;
-  rcT.channelValue[10] = 2000;
-  rcT.channelValue[11] = 1500; //RSSI (Signal Strength)
-  rcT.channelValue[12] = killswitch; //Killswitch
-  rcT.channelValue[13] = 1500;
-  rcT.channelValue[14] = 1500;
-  rcT.channelValue[15] = 1500;
-  msp.send(MSP_SET_RAW_RC, &rcT, sizeof(rcT));
-  if (wifiState == 1 && droneState == 1 && status == WL_CONNECTED){
-    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-    Udp.write("State: 1 -> 2");
-    Udp.endPacket();
-    wifiState = 2;
-  }
-  else if (wifiState == 2){ 
-    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-    Udp.write("AmDrone");
-    Udp.endPacket();
-    wifiState = 3;
-  }
-  else if (wifiState == 4) {
-    Udp.beginPacket(bsip, 5005);
-    Udp.write("HND|-1|NEWDRONE");
-    Udp.endPacket();
-    wifiState = 5; 
-  }
-  if(millis() - updateTime > 5000 && serialUSB)
-  {
-    Serial.println("<--------------------------->\nDrone Data");
-    Serial.print("Throttle: "); Serial.println(throttle);
-    Serial.print("Pitch: "); Serial.println(pitch);
-    Serial.print("Roll: "); Serial.println(roll);
-    Serial.print("Yaw: "); Serial.println(yaw);
-    Serial.print("Armed: "); Serial.println(isArmed);
-    Serial.print("Failsafe: "); Serial.println(isFailsafed);
-    Serial.print("Killswitch: "); Serial.println(isKilled);
-    Serial.println("<--------------------------->");
-    updateTime = millis();
-  }
-  // else if (state == 5) {
-  //   //call parsemanualcontrolmessage and process the results
-  //   //but do it in listen
-  //   // Serial.println(roll);
-  //   // Serial.println(yaw);
-  //   // Serial.println(pitch);
-  //   // Serial.println(throttle);
-  // }
-}//End Loop
-
-void DroneSystems(){
-  if (droneState == -1){//starting up
-    blinkSpeed = 1000;       
-    throttle = 885;
-    Arm(false);     
-    if(millis() - t > 1000){
-      t = millis();
-      droneState = 0;
-    }
-  }
-  else if (droneState == 0){//arm drone
-    blinkSpeed = 1000;
-    //set arming signals
-    Arm(true);
-    if (millis() - t > 500){
-      droneState = 1;
-      t = millis();
-    }
-  }
-  else if (droneState == 1) //nothing
-  {
-    //Roll Ch 0, pitch Ch 1, Yaw Ch 3, Throttle Ch 2, Arm Ch 4
-    
-  }
-  else if(droneState == 2)
-  {
-    
-    
-  }
-  if(status != WL_CONNECTED) 
-  {
-    if(millis()- blinkTime >= blinkSpeed){
-      LightSRLatch();
-      blinkTime = millis();
-    }
-  }
-  else if(droneState == -1 || droneState == 0){ //Light Blinker
-    if(millis()- blinkTime >= blinkSpeed){
-      LightSRLatch();
-      blinkTime = millis();
-    }
-  }
-  else if(killswitch == 1700){
-    digitalWrite(LEDpin,LOW);
-  }
-  else{
-    digitalWrite(LEDpin,HIGH);
-  }
-  CheckModeStates();
-  // delay(10);  
-}//End Drone Systems
-
-void Arm(bool armed)
-{
-  if(armed)
-  {
-    arming = 2000;
-  }
-  else if(!armed)
-  {
-    arming = 1000;
-  }
+void setup() {
+  delay(250);
+  Serial1.begin(9600);
+  Serial.begin(9600);
+  rc_values[0] = 1500;
+  rc_values[1] = 1500;
+  rc_values[2] = 885;
+  rc_values[3] = 1500;
+  rc_values[4] = 1500;
+  rc_values[5] = 1500;
+  rc_values[6] = 1500;
+  rc_values[7] = 1500;
 }
 
-void CheckModeStates(){ //sets booleans of the modes for enabled/disabled
-  if(arming > 1500)
+void loop() {
+  loopCount++;
+  uint8_t datad = 0;
+  uint8_t *data = &datad;
+  if(loopCount%2 == 0)
   {
-    isArmed = true;
+    commandMSP(MSP_SET_RAW_RC, rc_values, 16);
   }
   else
   {
-    isArmed = false;
-  }
-  if(failsafe > 1500)
-  {
-    isFailsafed = true;
-  }
-  else
-  {
-    isFailsafed = false;
-  }
-  if(killswitch > 1500)
-  {
-    isKilled = true;
-  }
-  else
-  {
-    isKilled = false;
+    sendMSP(MSP_RAW_GPS, 0, 0);
+    readGPSData();
+    sendMSP(MSP_ATTITUDE, data, 0);
+    readAttitudeData();
   }
 }
 
-int ChannelMath(double setpoint) //Math to set the same values as used in inav veiwer, < 0.0001% stray
+void commandMSP(uint8_t cmd, uint16_t data[], uint8_t n_cbytes)
 {
-  //channel limits assume setpoint between 900 - 2100
-  setpoint = (setpoint - 879.7)/.6251;
-  return round(setpoint);  
+
+  uint8_t checksum = 0;
+
+  Serial1.write((byte *) "$M<", 3);
+  Serial1.write(n_cbytes);
+  checksum ^= (n_cbytes);
+  Serial1.write(cmd);
+  checksum ^= cmd;
+  uint16_t cur_byte = 0;
+  while(cur_byte < (n_cbytes/2))
+  {
+    int8_t byte1 = ((uint16_t)data[cur_byte] >> 0) & 0xFF;
+    int8_t byte2 = ((uint16_t)data[cur_byte] >> 8) & 0xFF;
+    Serial1.write(byte1);
+    Serial1.write(byte2);
+    checksum ^= byte1;
+    checksum ^= byte2;
+    cur_byte++;
+  }
+  Serial1.write(checksum);
+  while(Serial1.available())
+  {
+    Serial1.read();
+    Serial.println("clear");
+  }
 }
 
-void LightSRLatch()
+void sendMSP(uint8_t req, uint8_t *data, uint8_t n_bytes) {
+
+    uint8_t checksum = 0;
+
+    Serial1.write((byte *) "$M<", 3);
+    Serial1.write(n_bytes);
+    checksum ^= n_bytes;
+
+    Serial1.write(req);
+    checksum ^= req;
+
+    Serial1.write(checksum);
+}
+
+void readAttitudeData() {
+  delay(100);
+  byte count = 0;
+
+  int16_t rollRec;
+  int16_t pitchRec;
+  int16_t yawRec;
+
+  while (Serial1.available()) {
+    count += 1;
+    byte first;
+    byte second;
+    switch (count) {
+      //first five bytes are header-type informatin, so start at 6
+    case 1 ... 2:
+      Serial1.read();
+      break;
+    case 3:
+      first = Serial1.read();
+      second = Serial1.read();
+      rollRec = second;
+      rollRec <<= 8;
+      rollRec += first;
+      break;
+    case 4:
+      first = Serial1.read();
+      second = Serial1.read();
+      pitchRec = second;
+      pitchRec <<= 8;
+      pitchRec += first;
+      break;  
+    case 5:
+      first = Serial1.read();
+      second = Serial1.read();
+      yawRec = second;
+      yawRec <<= 8;
+      yawRec += first;
+      break;
+    case 6:
+      Serial1.read();
+      break;
+    }
+  }
+  Serial.print("Roll: " + String(rollRec/10.0));
+  Serial.print(" Pitch: " + String(pitchRec/10.0));
+  Serial.println(" Yaw: " + String(yawRec));
+}
+
+void readGPSData()
 {
-  if(lightOn)
-  {
-    digitalWrite(LEDpin, LOW);
-    lightOn = false;    
-  }
-  else if(!lightOn)
-  {
-    digitalWrite(LEDpin, HIGH);
-    lightOn = true;    
-  }
-}
-
-void WifiConnection()
-{
-  // attempt to connect to Wi-Fi network:
-  if(status != WL_CONNECTED && ((millis() - connectTime) > 5000)) {
-    if(serialUSB)
-    {
-      Serial.print("Attempting to connect to network: ");
+  delay(100);
+  byte count = 0;
+  uint8_t gpsFix;
+  uint8_t numSat;
+  uint32_t lat;
+  uint32_t lon;
+  uint16_t gpsAlt;
+  uint16_t gpsSpeed;
+  uint16_t gpsCourse;
+  while (Serial1.available()) {
+    count += 1;
+    byte first;
+    byte second;
+    byte third;
+    byte fourth;
+    switch (count) {
+      //first five bytes are header-type informatin, so start at 6
+    case 1 ... 2:
+      Serial1.read();
+      break;
+    case 3: //Fix, 1 is yes, 0 is no
+      gpsFix = Serial1.read();
+      break;
+    case 4: //Number of satellites
+      numSat = Serial1.read();
+      break;  
+    case 5: //Combine latitude bytes
+      first = Serial1.read();
+      second = Serial1.read();
+      third = Serial1.read();
+      fourth = Serial1.read();
+      lat = fourth;
+      lat <<= 8;
+      lat += third;
+      lat <<= 8;
+      lat += second;
+      lat <<= 8;
+      lat += first;
+      break;
+    case 6: //Combine longitude bytes
+      first = Serial1.read();
+      second = Serial1.read();
+      third = Serial1.read();
+      fourth = Serial1.read();
+      lon = fourth;
+      lon <<= 8;
+      lon += third;
+      lon <<= 8;
+      lon += second;
+      lon <<= 8;
+      lon += first;
+      break;
+    case 7: //Altitude in meters
+      first = Serial1.read();
+      second = Serial1.read();
+      gpsAlt = second;
+      gpsAlt <<= 8;
+      gpsAlt += first;
+      break;
+    case 8: //Speed in cm/s
+      first = Serial1.read();
+      second = Serial1.read();
+      gpsSpeed = second;
+      gpsSpeed <<= 8;
+      gpsSpeed += first;
+      break;
+    case 9: //Degrees times 10
+      first = Serial1.read();
+      second = Serial1.read();
+      gpsCourse = second;
+      gpsCourse <<= 8;
+      gpsCourse += first;
+      break;
+    case 10:
+      Serial1.read();
+      break;
     }
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network:
-    status = WiFi.begin(ssid);
-    //Retry every 5 seconds
-    firstConnectFrame = true;
-    connectTime = millis();
   }
-  else if(status == WL_CONNECTED && firstConnectFrame)
-  {
-    Udp.begin(localPort);
-      // Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-    Udp.beginPacket("192.168.4.22", 80);
-    Udp.write("State: 0 -> 1 |Connected|");
-    Udp.endPacket();
-    wifiState = 1;
-    
-    Udp.beginPacket("192.168.4.22", 80);
-    Udp.write(ReplyBuffer);
-    Udp.endPacket();
-    // you're connected now, so print out the data:
-    if(serialUSB)
-    {
-      printBoardInfo();
-    }
-    firstConnectFrame = false;
-    bootComplete = true;
-  }
-}
-
-void SendMessage(char msg[]){
-  Udp.begin(localPort);
-  // Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-  Udp.beginPacket("192.168.4.22", 80);
-  Udp.write(msg);
-  Udp.endPacket();
-  Serial.println("Sent");
-  Serial.println(msg);
-  Serial.println("**********");
-}
-
-void printBoardInfo(){
-  Serial.println("Board Information:");
-  // print your board's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-
-  // print your network's SSID:
-  Serial.println();
-  Serial.println("Network Information:");
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.println(rssi);
-  Serial.println("---------------------------------------");
-  Serial.println("You're connected to the network");
-  Serial.println("---------------------------------------");
-
-}
-
-void MillisStuff() { //specifies whatever this stuff is for use in the loop, to make the looop easier to read
-  unsigned long currentMillisInfo = millis();
-
-  // check if the time after the last update is bigger the interval
-  if (currentMillisInfo - previousMillisInfo >= intervalInfo) {
-    previousMillisInfo = currentMillisInfo;
-
-  }
-  
-  unsigned long currentMillisLED = millis();
-  
-  // measure the signal strength and convert it into a time interval
-  int intervalLED = WiFi.RSSI() * -10;
- 
-  // check if the time after the last blink is bigger the interval 
-  if (currentMillisLED - previousMillisLED >= intervalLED) {
-    previousMillisLED = currentMillisLED;
-
-    // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW) {
-      ledState = HIGH;
-    } else {
-      ledState = LOW;
-    }
-
-    // set the LED with the ledState of the variable:
-    digitalWrite(LED_BUILTIN, ledState);
-  }
-}
-
-ManualControlMessage parseMessage(char buffer[]){
-  ManualControlMessage msg;
-  char *token;
-  token = strtok(buffer, "|");
-  int i = 0;
-  while(token != 0){
-    //Serial.println(token);
-    switch(i){
-      case 0:
-        msg.cmd = token;
-        break;
-      case 1:
-        msg.sourceIP = token;
-        break;
-      case 2:
-        msg.yaw = atoi(token);       
-        break;
-      case 3:
-        msg.pitch = atoi(token);  //pitch
-        break;
-      case 4: 
-        msg.roll = atoi(token);
-        break;
-      case 5:
-        msg.throttle = atoi(token);
-        break;
-      case 6:
-        msg.killswitch = atoi(token);
-        break;
-      case 7:
-        msg.pytime = atoi(token);
-        break;
-      }
-      roll = msg.roll;
-      pitch = msg.pitch;
-      throttle = msg.throttle;
-      yaw = msg.yaw;
-      killswitch = msg.killswitch;
-      pytime = msg.pytime;
-    i++;
-    token = strtok(NULL, "|"); 
-  }
-return msg;  
-//HAS REQUIRED PACKETS FROM LISTEN, CODE FOR MANUAL MODE HERE --------------
-//Currently does not include a break, repeats loop forever
-}  
-
-BSIPMessage parseBSIP(char buffer[]){
-  BSIPMessage msg;
-  char *token;
-  token = strtok(buffer, "|");
-  int i = 0;
-  Serial.println("In parseBSIP");
-  while(token != 0){
-    Serial.println(token);
-    switch(i){
-      case 0:
-        msg.cmd = token;
-        break;
-      case 1:
-        msg.BSIP = token;
-        break;        
-      }
-    i++;
-    token = strtok(NULL, "|");
-    
-  }
-  return msg;  
-}  
-
-void Listen(){
-  int packetSize = Udp.parsePacket();
-  if(packetSize){
-    //Serial.print("Received packet of size ");
-    //Serial.println(packetSize);
-    //Serial.print("From ");
-    IPAddress remoteIp = Udp.remoteIP();
-    //Serial.print(remoteIp);
-    //Serial.print(", port ");
-    //Serial.println(Udp.remotePort());
-    // read the packet into packetBufffer
-    int len = Udp.read(packetBuffer, 255);
-    //Serial.println(packetBuffer);
-    if (len > 0) {
-      packetBuffer[len] = 0;
-      if (wifiState == 3){
-        Serial.println("Parsing BSIP Message");
-        //read BSID response from AP      
-        BSIPMessage msg = parseBSIP(packetBuffer);
-        if (msg.cmd == "BSIP"){
-          bsip = msg.BSIP;
-          Serial.print("Base Station IP: ");
-          Serial.println(bsip);
-          wifiState = 4;
-        }
-      }
-      else if (wifiState == 5){
-        //Serial.println("listening for manual message");
-        ManualControlMessage msg = parseMessage(packetBuffer);
-        if(serialUSB)
-        {
-          Serial.print("packet: ");
-          Serial.print(msg.throttle);
-          Serial.print(" recived at: ");
-          Serial.print(millis());
-        }
-        if (msg.cmd == "MAN"){
-          roll = msg.roll;
-          pitch = msg.pitch;
-          throttle = msg.throttle;
-          yaw = msg.yaw;
-          ();
-        }
-      }
-    }    
-  }
+  Serial.print("Fix: " + String(gpsFix));
+  Serial.print(" NumSat: " + String(numSat));
+  Serial.print(" Lat: " + String(lat/10000000));
+  Serial.print(" Lon: " + String(lon/10000000));
+  Serial.print(" GPSALT: " + String(gpsAlt));
+  Serial.print(" SOG: " + String(gpsSpeed));
+  Serial.println(" GPSCourse: " + String(gpsCourse/10));
 }
